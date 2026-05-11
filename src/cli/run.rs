561edb64031067c6044b68f07942f2a651557efb
et-nik <nikita.hldm@gmail.com>
2026-05-11 02:01:18 +0200

    fix

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -171,9 +171,10 @@ async fn run_server_loop(
             args.backup_dir
                 .clone()
                 .or(config.defaults.backup.path.clone());
-        let backup_frequency = args
-            .backup_frequency
-            .or((config.defaults.backup.frequency != 0).then_some(config.defaults.backup.frequency));
+        let backup_frequency =
+            args.backup_frequency
+                .or((config.defaults.backup.frequency != 0)
+                    .then_some(config.defaults.backup.frequency));
 
         // Run the server
         let exit_code = runner
